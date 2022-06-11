15b1f34e8d4849a74d37b6a8ef32d677a70d61fa
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-10 21:11:05 -0500

    Se crea el servicio para validar el Token

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos, validarJWT } = require('../middlewares');
 
 
-const { login, googleSignin } = require('../controllers/auth');
+const { login, googleSignin, renovarToken } = require('../controllers/auth');
 
 
 const router = Router();
@@ -14,12 +14,14 @@ router.post('/login',[
     check('correo', 'El correo es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
-],login );
+], login );
 
 router.post('/google',[
     check('id_token', 'El id_token es necesario').not().isEmpty(),
     validarCampos
 ], googleSignin );
+
+router.get('/', validarJWT, renovarToken );
 
 
 
