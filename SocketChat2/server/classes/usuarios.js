b1a245a96ccafac8d26ea8b997162fe7090cddd4
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-23 15:46:27 -0500

    Clase para controlar los usuarios del chat

---
+++ SocketChat2/server/classes/usuarios.js
@@ -0,0 +1,34 @@
+class Usuarios {
+    constructor(){
+        this.personas = [];
+    }
+
+    agregarPerson(id, nombre){
+        let persona = { id, nombre };
+        this.personas.push(persona);
+        return this.personas;
+    }
+
+    getPersona(id){
+        let persona = this.personas.filter( person => person.id === id )[0];
+        return persona;
+    }
+
+    getPersonas(){
+        return this.personas;
+    }
+
+    getPersonaBySala(){
+        // #TODO:
+    }
+
+    borrarPersona(id){
+        let personaBorrada= this.getPersona(id);
+        this.personas= this.personas.filter( persona => persona.id !== id );
+        return personaBorrada;
+    }
+}
+
+module.exports= {
+    Usuarios
+}
